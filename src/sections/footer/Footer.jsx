65f570b917be67c8a7f9723631f6bdae93f8ef5a
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-03-21 15:05:20 -0400

    finished footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -3,8 +3,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="container footer__container  ">
-        <p>Designed by Isaac Yu</p>
-        <small>built with: React.js html css</small>
+        <a href="https://github.com/IsaacYu15/portfoliowebsite"  target="_blank" rel="noopener noreferrer">Designed by Isaac Yu <br/> built with: React.js html css</a>
       </div>
 
     </footer>
