234ec34e5cccd08455358c7e5c031eaf9938c852
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-03-21 19:04:20 -0400

    clean up folders

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <section id="contact">
       <div className="container contact__container">
         <h2>Let's Get In Touch!</h2>
-        <p>Feel free to reach out wheter you want to just say hi or any questions! I'm always open to new opportunities, mentorship and am currently on the look out for a summer internship.</p>
+        <p>Feel free to reach out wheter you want to just say hi or ask any questions! I'm always open to new opportunities, mentorship and am currently on the look out for a summer internship.</p>
         <a href = 'mailto:yuisaac@outlook.com' className='btn primary'>Say Hi!</a>
       </div>
 
