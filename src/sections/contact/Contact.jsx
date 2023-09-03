3c2b5886db395fde76558718cbdd8d0a41a45c9a
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-09-03 18:07:21 -0400

    testing update gh pages

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
       <div className="container contact__container">
         <h2>Let's Get In Touch!</h2>
         <p>Feel free to reach out and say hi!</p>
+        <p>Whenever you want</p>
         <a href = 'mailto:yuisaac@outlook.com' className='btn primary'>Say Hi!</a>
       </div>
 
