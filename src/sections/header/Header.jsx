2d66897ae2f250542ee6b0318a08cc524d2f0ed4
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-09-05 23:24:10 -0400

    debug vercel

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -5,24 +5,7 @@ import data from './data'
 const Header = () => {
   return (
     <header id="header">
-      <div className="container header__container">
-
-        <div className = "headerContent">
-           <h1 className="headerBase">ISAAC YU</h1>  
-           <h1 className="headerOverlay">ISAAC YU</h1>  
-        </div>
-
-
-        <img className="profile" src={HeaderImage} alt="Portrait"/>
-        
-        <div className='header__socials'>
-          {
-            //iterate through all the items in data, setting the link to the name
-            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
-          }
-          </div> 
-
-      </div>
+     
 
     </header>
   )
