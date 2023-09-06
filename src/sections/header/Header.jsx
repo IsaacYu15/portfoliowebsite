0577e76e538a60ee453a145070de72ba5455ff44
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-09-05 23:30:14 -0400

    debug vercel

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,5 +1,5 @@
 import'./header.css'
-import HeaderImage from '../../assets/headerImage.png'
+//import HeaderImage from '../../assets/headerImage.png'
 import data from './data'
 
 const Header = () => {
@@ -10,7 +10,7 @@ const Header = () => {
         <h1 className="headerBase">ISAAC YU</h1>  
         <h1 className="headerOverlay">ISAAC YU</h1>  
 
-        <img className="profile" src={HeaderImage} alt="Portrait"/>
+        <img className="profile" src='../../assets/headerImage.png' alt="Portrait"/>
         
         <div className='header__socials'>
           {
