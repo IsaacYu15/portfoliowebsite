e0fd9490421e894e722a1f15d1d6a772ad62824a
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-09-06 19:35:12 -0400

    feat: update and reorganized header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -13,13 +13,19 @@ const Header = () => {
         <img className="profile" src={HeaderImage} alt="Portrait"/>
         
         <div className='header__socials'>
-          {
-            //iterate through all the items in data, setting the link to the name
-            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
-          }
-          </div> 
+        {
+          //iterate through all the items in data, setting the link to the name
+          data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+        }
+        </div> 
 
       </div>
+
+      <div className="container bio__container  "> 
+          <p>Hey! 👋 My names Isaac and I'm studying System Design Engineering at Waterloo. My passion for coding started
+          with making video games, but now I've started to explore other projects including
+          the embed software and websites! </p>  
+        </div>
 
     </header>
   )
