524b4aa0a1b7b55a77f10e1b8c5639d0618f6f76
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-09-06 19:06:58 -0400

    update path

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,5 +1,5 @@
 import'./header.css'
-import HeaderImage from '../../assets/headerImage.PNG' 
+import HeaderImage from '../../assets/headerImage.png' 
 import data from './data'
 
 const Header = () => {
