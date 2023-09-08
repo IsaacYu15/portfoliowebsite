a9c6ac3835f5d545948690a00497f64bb57cd7eb
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-09-07 22:39:09 -0400

    feat: update work section

--- src/sections/work/data.js
+++ src/sections/work/data.js
@@ -2,16 +2,35 @@
 
 const data = [
 
-     {id: 1, source:'SplatShowcase', 
-     title: 'SPLAT!', 
-     desc: 'Developed and released a 2D platform where player shrinks with every jump. ' +
-           'Over 30 levels, endless mode, skins and more available!',
-     lang: 'Unity, C#',
-     link: 'https://play.google.com/store/apps/details?id=com.IsaacYu.Splat',
-     git: 'N/A'
-    },
+      {id: 1, source:'SpaceoutShowcase', 
+      title: 'SPACE OUT!', 
+      desc: 'Developed a spaced repitition flashcard game that rewards you with a mix and match game every time' +
+            'you master a card. Won 1st Place Overall & Best Productivity/Time Saving Hack (Clock Hacks)',
+      lang: 'Unity, C#',
+      link: 'https://devpost.com/software/spaced-out-fbv7tz',
+      git:  'https://github.com/IsaacYu15/ClockHacks_SPACEOUT'
+      },
+  
+      {id: 2, source:'wordhuntsolverShowcase', 
+      title: 'WORDHUNT / BOGGLE SOLVER', 
+      desc: 'Developed a recursive algorithm that finds all valid words in a 4x4 grid (valid words being a' +
+            'word in the dictionary and all letters are diagonal, above/below, or beside one another',
+      lang: 'Javascript, HTML, CSS',
+      link: 'https://word-hunt-solver-eight.vercel.app/',
+      git:  'https://github.com/IsaacYu15/WordHuntSolver'
+      },
 
-     {id: 2, source:'ProceduralShowcase', 
+      {id: 3, source:'vrsurgeryShowcase', 
+      title: 'VR SURGERY', 
+      desc: 'Developed a surgery game that allows users to experience the basic features of surgery robots ' +
+            'such as grabbing, cutting and suturing.',
+      lang: 'Unity, C#, Oculus Integration',
+      link: 'https://www.youtube.com/watch?v=iJSWnNqPqfo',
+      git:  'https://github.com/IsaacYu15/VR-Surgery'
+      },
+
+
+     {id: 4, source:'ProceduralShowcase', 
      title: 'PROCEDURAL ANIMATION', 
      desc: 'Developed an algorithm which generates walking animations in real time by using vector math. ' +
            'This allows spider can walk over uneven terrains realistically.',
@@ -20,23 +39,17 @@ const data = [
      git: 'https://github.com/IsaacYu15/ProceduralWalkingAnimation'
     },
 
-    {id: 3, source:'vrsurgeryShowcase', 
-    title: 'VR SURGERY', 
-    desc: 'Developed a surgery game that allows users to experience the basic features of surgery robots ' +
-          'such as grabbing, cutting and suturing.',
-    lang: 'Unity, C#, Oculus Integration',
-    link: 'https://www.youtube.com/watch?v=iJSWnNqPqfo',
-    git:  'https://github.com/IsaacYu15/VR-Surgery'
-    },
-
-    {id: 4, source:'SpaceoutShowcase', 
-    title: 'SPACE OUT!', 
-    desc: 'Developed a spaced repitition flashcard game that rewards you with a mix and match game every time' +
-          'you master a card. Won 1st Place Overall & Best Productivity/Time Saving Hack (Clock Hacks)',
+    {id: 5, source:'SplatShowcase', 
+    title: 'SPLAT!', 
+    desc: 'Developed and released a 2D platform where player shrinks with every jump. ' +
+          'Over 30 levels, endless mode, skins and more available!',
     lang: 'Unity, C#',
-    link: 'https://devpost.com/software/spaced-out-fbv7tz',
-    git:  'https://github.com/IsaacYu15/ClockHacks_SPACEOUT'
-    },
+    link: 'https://play.google.com/store/apps/details?id=com.IsaacYu.Splat',
+    git: 'N/A'
+   },
+
+
+
 
 
 
