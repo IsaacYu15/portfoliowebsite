50df4b2e50a8a598cb095f4652f84e6a0a19aa78
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-11-26 00:59:32 -0500

    feat: revised header page

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,50 +1,54 @@
-import'./header.css'
-import HeaderImage from '../../assets/headerImage.png' 
-import data from './data'
+import "./header.css";
+import { useEffect } from "react";
 
-const scrollToBio =()=> {
+const scrollToBio = () => {
   const div = document.getElementsByClassName("container bio__container");
 
   div[0].scrollIntoView({
     block: "center",
     inline: "center",
   });
-
-}
+};
 
 const Header = () => {
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      var mouseX = (e.clientX / window.innerWidth) * 100;
+      var mouseY = (e.clientY / window.innerHeight) * 100;
+      var header = document.getElementsByClassName("headerText");
+      var pos = (mouseX / 2 + mouseY / 2).toString() + "% ";
+
+      for (var i = 0; i < header.length; i++) {
+        header[i].style.backgroundPosition = pos;
+      }
+
+      console.log(header[0]);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
-
-
     <header id="header">
-
       <div className="container header__container">
-
-        <h1 className="headerBase">ISAAC YU</h1>  
-        <h1 className="headerOverlay">ISAAC YU</h1>  
-
-        <img className="profile" src={HeaderImage} alt="Portrait"/>
-        
-        <div className='header__socials'>
-        {
-          //iterate through all the items in data, setting the link to the name
-          data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
-        }
-        </div> 
-
+        <h1 className="headerText">ISAAC YU</h1>
+        <h1 className="headerText">ISAAC YU</h1>
+        <h1 className="headerText">ISAAC YU</h1>
       </div>
 
       <button className="arrow" onClick={scrollToBio}></button>
 
-      <div className="container bio__container  "> 
-          <p>Hey! 👋 My name is Isaac and I'm studying Systems Design Engineering at Waterloo. I've been developing games for 
-            5+ years, and have been exploring various applications programming in the real world. I'm always on the look out to learn new things
-            such as working with embeded software in a research lab to learning 
-            full stack development! </p>  
+      <div className="container bio__container  ">
+        <p>
+          Hey! 👋 My name is Isaac and I'm studying Systems Design Engineering
+          at Waterloo. I've been developing games for 5+ years, and have been
+          exploring various applications programming in the real world. I'm
+          always on the look out to learn new things such as working with
+          embeded software in a research lab to learning full stack development!{" "}
+        </p>
       </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
