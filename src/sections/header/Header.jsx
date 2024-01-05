fb4e8cd7febf05ca3966d255328954e2f6a11a11
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2024-01-05 13:45:03 -0500

    feat: reworked css in experiences

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,41 +1,6 @@
 import "./header.css";
-import { useEffect } from "react";
 
 const Header = () => {
-  const scrollToBio = () => {
-    const div = document.getElementsByClassName("container bio__container");
-
-    div[0].scrollIntoView({
-      block: "center",
-      inline: "center",
-    });
-  };
-  /*
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      var mouseX = (e.clientX / window.innerWidth) * 100;
-      var mouseY = (e.clientY / window.innerHeight) * 100;
-      var header = document.getElementsByClassName("headerText");
-      var pos = (mouseX / 2 + mouseY / 2).toString() + "% ";
-
-      for (var i = 0; i < header.length; i++) {
-        header[i].style.backgroundPosition = pos;
-      }
-
-      console.log(header[0].style);
-    };
-
-    document.addEventListener("mousemove", handleMouseMove);
-
-    window.addEventListener("scroll", () => {
-      document.body.style.setProperty(
-        "--scroll",
-        window.scrollY / window.innerHeight
-      );
-    });
-  }, []);
-  */
-
   return (
     <header id="header">
       <div class="bg"></div>
